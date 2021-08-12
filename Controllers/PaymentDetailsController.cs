154a0dbe9687a8c851cbdbb5bf3581967ee5f561
Selvakumar2000 <skselva312000@gmail.com>
2021-08-12 12:26:18 +0530

    "Bank Application API Completed"

---
+++ Controllers/PaymentDetailsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankApplication.Models;
+
+namespace BankApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailsContext _context;
+
+        public PaymentDetailsController(PaymentDetailsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> GetPaymentDetail()
+        {
+            return await _context.PaymentDetail.ToListAsync();
+        }
+
+        // GET: api/PaymentDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetails>> GetPaymentDetails(int id)
+        {
+            var paymentDetails = await _context.PaymentDetail.FindAsync(id);
+
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetails;
+        }
+
+        // PUT: api/PaymentDetails/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetails(int id, PaymentDetails paymentDetails)
+        {
+            if (id != paymentDetails.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetails
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetails>> PostPaymentDetails(PaymentDetails paymentDetails)
+        {
+            _context.PaymentDetail.Add(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetails", new { id = paymentDetails.PaymentDetailId }, paymentDetails);
+        }
+
+        // DELETE: api/PaymentDetails/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaymentDetails>> DeletePaymentDetails(int id)
+        {
+            var paymentDetails = await _context.PaymentDetail.FindAsync(id);
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetail.Remove(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return paymentDetails;
+        }
+
+        private bool PaymentDetailsExists(int id)
+        {
+            return _context.PaymentDetail.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
---
+++ Models/PaymentDetailsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApplication.Models
+{
+    //inherit entity framework class
+    public class PaymentDetailsContext : DbContext
+    {
+        //database provider and connection string
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PaymentDetails> /*table name*/PaymentDetail { get; set; }
+
+    }
+}
